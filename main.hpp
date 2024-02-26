c2248cc62156c3c3113697f50066798e2dd48973
Antonio Narino <anarino@students.avc.edu>
2024-02-25 21:10:21 -0800

    testing splitline function

--- main.hpp
+++ main.hpp
@@ -8,6 +8,22 @@ const int MAXLEN = 100;
 
 int splitline(string, char, char[][MAXLEN]);
 
-/*******************************
- * Code your program here
- *******************************/
+int splitline(string x, char y, char split[][MAXLEN]) {
+    int i, j, k;
+    int cnt = 0;
+
+    i = 0;
+    while(i < x.length()) {
+        if((x.at(i) != y) && (x.at(i) != '\0'))
+            split[j][k++] = x.at(i);
+        else {
+            split[j][k] = '\0';
+            j++;
+            k = 0;
+            cnt++;
+        }
+        i++;
+    }
+    
+    return cnt;
+}
